942a728ef797627a3f6abeb330738b866a829b47
Isac Ferreira <aristocrata.black@gmail.com>
2026-06-08 19:19:55 -0300

    dasdaw

--- src/components/meta-pixel.tsx
+++ src/components/meta-pixel.tsx
@@ -40,6 +40,16 @@ fbq('init', '${FB_PIXEL_ID}');`}
           alt=""
         />
       </noscript>
+
+      {/* UTMify Pixel */}
+      <Script id="utmify-pixel" strategy="afterInteractive">
+        {`window.pixelId = "6a273f9aae1acc029476e1ea";
+var a = document.createElement("script");
+a.setAttribute("async", "");
+a.setAttribute("defer", "");
+a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+document.head.appendChild(a);`}
+      </Script>
     </>
   );
 }
